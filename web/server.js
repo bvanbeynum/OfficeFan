7dd89757ded2869462e7bebf9253a4742f87812b
Brett van Beynum <maildrop444@gmail.com>
2020-07-19 00:57:32 -0400

    Adding light to the web page

--- web/server.js
+++ web/server.js
@@ -14,13 +14,13 @@ app.set("root", __dirname);
 app.use(bodyParser.json());
 
 var config = require("./config");
-mongoose.set('bufferCommands', false);
 mongoose.connect("mongodb://" + config.mongo.user + ":" + config.mongo.pass + "@" + config.mongo.servers.join(",") + "/" + config.mongo.db + "?authSource=" + config.mongo.db, {useNewUrlParser: true});
 
 var sensorModel = mongoose.model("sensor", {
 	logTime: Date,
 	temp: Number,
-	humidity: Number
+	humidity: Number,
+	isLightOn: Boolean
 });
 
 // Routes =======================================================================
@@ -35,7 +35,8 @@ app.post("/api/savesensor", (request, response) => {
 	new sensorModel({
 		logTime: new Date(request.body.sensor.logTime),
 		temp: request.body.sensor.temp,
-		humidity: request.body.sensor.humidity
+		humidity: request.body.sensor.humidity,
+		isLightOn: request.body.sensor.light == 1 ? true : false
 	})
 	.save()
 	.then((sensorDB) => {
@@ -48,8 +49,41 @@ app.post("/api/savesensor", (request, response) => {
 	});
 });
 
+app.get("/api/sensorload", (request, response) => {
+	var filterDate = new Date();
+	filterDate.setHours(filterDate.getHours() - 12);
+
+	sensorModel
+		.find({
+			logTime: { $gte: filterDate }
+		})
+		.exec()
+		.then((sensorsDb) => {
+			var output = sensorsDb.map((sensorDb) => {
+				return {
+					id: sensorDb._id,
+					logTime: sensorDb.logTime,
+					temp: sensorDb.temp,
+					humidity: sensorDb.humidity,
+					isLightOn: sensorDb.isLightOn || false
+				};
+			});
+			
+			response.status(200).json({ sensor: output });
+			response.end();
+		})
+		.catch((error) => {
+			response.status(500).json({error: error.message});
+			response.end();
+		});
+});
+
 app.get("/test", function(request, response) {
 	response.sendFile("/client/test.html", { root: app.get("root") });
+});
+
+app.get("/sensor", function(request, response) {
+	response.sendFile("/client/sensor.html", { root: app.get("root") });
 });
 
 app.get("*", (request, response) => {
