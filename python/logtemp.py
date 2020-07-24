f1fca8eb986f042808f8c0ee8b11591382320059
Brett van Beynum <maildrop444@gmail.com>
2020-07-24 10:21:28 -0400

    added fan control

--- python/logtemp.py
+++ python/logtemp.py
@@ -5,10 +5,40 @@ import datetime
 import requests
 import json
 
+# Arduino command mapping
+#	Fan
+#		1 = fan on
+#		2 = fan off
+
 port = glob.glob("/dev/ttyUSB*")[0]
 arduino = serial.Serial(port, 9600)
 
 while True:
-	data = json.loads(arduino.readline())
-	data["logTime"] = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-	requests.post("http://huntingtonbeach.beynum.com:8081/api/savesensor", json = { "sensor": data })
+	if arduino.in_waiting > 0:
+		data = json.loads(arduino.readline())
+		data["logTime"] = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+		requests.post("http://huntingtonbeach.beynum.com:8081/api/savesensor", json = { "sensor": data })
+
+	response = requests.get("http://huntingtonbeach.beynum.com:8081/api")
+
+	if response:
+		commands = response.json()["commands"]
+		commandTypes = []
+
+		# Get the last status if there are multiple statuses for each type
+		for command in commands:
+			if any(commandType["type"] == command["type"] for commandType in commandTypes):
+				commandType = next(commandType for commandType in commandTypes if commandType["type"] == command["type"])
+				commandType["status"] = command["status"]
+			else:
+				commandTypes.append({ "type": command["type"], "status": command["status"] })
+
+		for commandType in commandTypes:
+
+			if commandType["type"] == "fan":
+				if commandType["status"]:
+					arduino.write(str.encode("1"))
+				else:
+					arduino.write(str.encode("2"))
+
+	time.sleep(1)
